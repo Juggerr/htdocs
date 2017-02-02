c8f582b0da650bd7103f9bd3bb398913af40e882
alex95kr <alex95kr@gmail.com>
2017-02-02 21:41:11 +0200

    Home

--- test.php
+++ test.php
@@ -16,6 +16,38 @@ if(strpos($exm, 'http://') === false) {
 	$exm = trim($exm, '/');
 }
 
-print $exm;
+//print $exm;
+
+$text = 'президент Петр Порошенко, отвечая на вопрос о том, как он относится к позиции как президент США Дональд Трамп отменит антироссийские санкции, заявил, что больше всех желает этого, однако условия должны быть соблюдены. Также в переговорах участвовал президент Путин и его соратник президент Лушашенко';
+$sinonim = array ('мусье', 'товарищь', 'вельми понеже', 'властелин', 'повелитель мира');
+$change = 'президент';
+
+$arr = explode(' ', $text);
+foreach ($arr as $value)
+{
+	print $arr[$value];
+	if ($value == $change)
+	{
+		$arr[$value] = 'alibaba';
+	}
+}
+
+var_dump($arr);
+
+
+/*
+//var_dump($sinonim);
+$num = substr_count($text, $change);
+//print $num;=
+for ($num; $num >= 0; --$num) 
+{
+print $num;
+$result = str_replace($change, $sinonim[array_rand($sinonim)], $text);
+
+}
+print $result;
+*/
 
 ?>
+// разобрать через имплоде
+.. сделать функцию стр_реплайс_ванс
