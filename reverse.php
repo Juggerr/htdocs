2edbe8f071b479f5f8af5caae69e3fb73e9947b1
alex95kr <alex95kr@gmail.com>
2017-02-22 11:43:33 +0200

    work

--- reverse.php
+++ reverse.php
@@ -1,8 +1,8 @@
 <?php
 
-$sometext = 'albania-forewer'; - исходная строка
-$reversetext = ''; - тут будет перевернутая строка
-$count = strlen($sometext); - вычисляем длинну строки
+$sometext = 'albania-forewer'; //- исходная строка
+$reversetext = ''; //- тут будет перевернутая строка
+$count = strlen($sometext); //- вычисляем длинну строки
 $count2 = $count; 
 $count2--; 
 
@@ -10,7 +10,7 @@ for ($i = 0; $i < $count; $i++)
 {
 @$reversetext[$i] .= $sometext[$count2--];
 }
-$reversetext = implode($reversetext); - собираем строку из массива
+$reversetext = implode($reversetext); //- собираем строку из массива
 
 print($reversetext);
 
