c0ac37b994001a2206e2dbe8d80101cd163e36e3
alex95kr <alex95kr@gmail.com>
2017-02-22 07:32:55 +0200

    home

--- array.php
+++ array.php
@@ -1,5 +1,5 @@
 <?php
-$starttime = time()+microtime();
+//$starttime = time()+microtime();
 /*
 массивы
 
@@ -8,17 +8,35 @@ array('color' => 'red') - создание ассоциативного масс
 array_push($array, $value, $value2...) - добавляет в массив значение в последнюю позицию (массив работает как стек)
 array_pop($array, $value) - выталкивает последнее значение из массива (по типу стека) и возвращает его как значение
 list($var, $var2, $var3) = $array - разбить массив на отдельные переменные
-rangre(2,20, 1) -  создает массив с диапазоном чисел 2-20 и шагом 1
+range(2,20, 1) -  создает массив с диапазоном чисел 2-20 и шагом 1
 reset($array) — Устанавливает внутренний указатель массива на его первый элемент
 each($array) - возращает текушую пару ключ-значение массива
 key() возвращает индекс текущего элемента массива ?? value
 current() — Возвращает текущий элемент массива ?? index
 next() — Передвигает внутренний указатель массива на одну позицию вперёд
+
 array_map(call_back_function, $array) example $upperarray = array_map('strtoupper', $array) обработка каждого элемента массива
+
+array_walk($array, callback func) -  применяет каллбек функцию к каждому элементу массива - работает по типу foreach те с копией массива
+
 is_array() - определяет массив ли это
 settype() - присваивает переменной новый тип settype($somevars, 'array') - преобразование всех переменных в массив
 unset() - удаляет переменную или элемент массива
+
 array_splice($array, $offset - c какого индекса начнется удаление, $length - сколько удалять) — Удаляет часть массива и заменяет её чем-нибудь ещё
+array_splice($array, 2, 2) вырезание из архива с 2 позиции, 2 ключ+значение
+
+array_values() — если массив ассоциативный то функция приводит к индексным ключам весь массив т.е. было 'aaa' => 'a' стало 01 => 'a';
+
+
+array_shift() -  извлечь первое значение массива и возращает его скращая на 1
+
+array_pop() - извлекает последнее значение массива и возращает его сокращая на 1
+
+$newarray = array_pad($array, 5, '') - расшираяет массив до 5 двумя пробелами
+если значение -5 то добавляет вначало массива еще 4 значений
+
+
 
 foreach ( $myArray as &$value - &cслыка на значение если нужно поменять данные в массиве
 
@@ -36,10 +54,21 @@ foreach ($array as $key => $value)
 
 */
 
+$testarray = range(0, 100, 1);
+
+$newArray = array_slice($testarray, 80, 20);
+$count = count($newArray);
+$newArray = array_pad($newArray, 10, "d");
+
+$count2 = count($newArray);
+print_r($newArray);
+echo "{$count} - before: {$count2}";
+
+
 
 $arrayz = array (
 1 => 'a',
-2 => 'b',
+'test' => 'b',
 3 => 'c',
 4 => 'd',
 5 => 'f',
@@ -57,6 +86,7 @@ foreach ($arrayz as $key => $value)
 	//echo "new value is {$value}</br>";
 }	
 
+$some = array_values($arrayz);
 
 $colorArray = ['red', 'white', 'green', 'blue', 'orange'];
 $brandArray = ['mercedes', 'bmv', 'audi', 'jeep', 'toyota'];
@@ -78,7 +108,7 @@ for ($i = 0; $i < 5; $i++)
 
 foreach($arrayTwoDimension as $key => $value)
 {
-	echo $key;
+	//echo $key;
 	foreach($value as $key => $value)
 	{
 	//echo "{$key} - {$value}</br>";
@@ -90,14 +120,10 @@ $fruits = array();
 $fruits['red'][] = 'strabbery';
 $fruits['red'][] = 'apple';
 $fruits['yellow'][] = 'banana';
-print_r($fruits);
-
-$testarray = range(0, 500, 5);
-$count = count($testarray);
 
 
-//echo next($testarray);
-echo current($testarray);
+
+
 
 
 
@@ -110,7 +136,6 @@ echo current($testarray);
 
 
 
-echo $arrayTwoDimension[0]["color"];
 
 $test = array('alfa', 'betta', 'omega');
 $test2[0] = 'alfa2';
@@ -123,7 +148,7 @@ $test3 = [
 'type' => 'bmv',
 'cost' => 20000
 ];
-// --------------------- это очень странная конструкция ассоциативного массива
+/* --------------------- это очень странная конструкция ассоциативного массива
 for ($i = 0; $i < 10; $i++)
 {
 	$test3['id'.$i] = $i;
@@ -133,7 +158,7 @@ for ($i = 3; $i < 8; $i++)
 {
 	echo $test3['id'.$i]; 
 }
-//-------------------------
+*/
 
 
 $test4 = array('color' => 'green', 'type' => 'mercedes', 'cost' => 40000);
@@ -149,8 +174,8 @@ echo $value;
 }
 */
 
-$stoptime = time()+microtime();
-$gentime = round($stoptime-$starttime, 4);
-print "Page created in $gentime seconds.";
+//$stoptime = time()+microtime();
+//$gentime = round($stoptime-$starttime, 4);
+//print "Page created in $gentime seconds.";
 
 ?>
