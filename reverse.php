bbc2577fca21692deaf0a83cc8c593f24793c005
Igor Lyakh <igor.lya@producters.com>
2017-02-22 12:21:23 +0200

    fixed issues

--- reverse.php
+++ reverse.php
@@ -3,14 +3,18 @@
 $sometext = 'albania-forewer'; //- исходная строка
 $reversetext = ''; //- тут будет перевернутая строка
 $count = strlen($sometext); //- вычисляем длинну строки
-$count2 = $count; 
-$count2--; 
+$count2 = $count;
+$count2--;
 
 for ($i = 0; $i < $count; $i++)
 {
-@$reversetext[$i] .= $sometext[$count2--];
+// @$reversetext[$i] .= $sometext[$count2--];
+$reversetext .= $sometext[$count2--]; // Пускай это будет просто конкатенация 
 }
-$reversetext = implode($reversetext); //- собираем строку из массива
+
+// Так как мы сделали переменную $reversetext строчкой, то не нужно
+// делать implode() (собирать массив в один)
+// $reversetext = implode($reversetext); //- собираем строку из массива
 
 print($reversetext);
 
